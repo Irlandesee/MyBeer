c4b704678445da234dea89e5183fe3c7d860207f
Irlandesee <mattia.lunardi@gmail.com>
2024-10-03 10:57:25 +0200

    Added loading picture functionality

--- app/src/main/java/it/uninsubria/mybeer/adapters/BeerListAdapter.kt
+++ app/src/main/java/it/uninsubria/mybeer/adapters/BeerListAdapter.kt
@@ -8,8 +8,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import it.uninsubria.mybeer.R
 import it.uninsubria.mybeer.datamodel.Beer
+import kotlinx.coroutines.withContext
 
 class BeerListAdapter(private var beerList: ArrayList<Beer?>):
     RecyclerView.Adapter<BeerViewHolder>(){
@@ -26,6 +29,8 @@ class BeerListAdapter(private var beerList: ArrayList<Beer?>):
             holder.tvBeerBrewery.text = beerList[position]?.beer_brewery
             holder.tvBeerAbv.text = beerList[position]?.beer_abv
             holder.tvBeerDesc.text = beerList[position]?.beer_desc
+            holder.bind(beerList[position]?.beer_picture_link)
+
         }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -45,7 +50,19 @@ class BeerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
     val tvBeerDesc: TextView = itemView.findViewById(R.id.tvBeerDesc)
     val ivPicture: ImageView = itemView.findViewById(R.id.iv_picture)
 
+    fun bind(beerPictureLink: String?){
+        val requestOptions = RequestOptions()
+            .placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_launcher_background)
+        Glide.with(itemView)
+            .applyDefaultRequestOptions(requestOptions)
+            .load(beerPictureLink)
+            .into(ivPicture)
+
+    }
+
     init{
+
         itemView.setOnClickListener{
             //listener.onClick()
         }
